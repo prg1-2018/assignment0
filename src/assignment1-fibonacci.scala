37e1d86dddac9d00f5dcb035e840171523ce6d28
Sasagawa Takuya <sasagawa.t.ae@is-mac25.edu.c.titech.ac.jp>
2018-10-02 14:08:45 +0900

    saisho no 2gyo comment out & itr yosaso

--- src/assignment1-fibonacci.scala
+++ src/assignment1-fibonacci.scala
@@ -1,5 +1,5 @@
-package prg1.assignment1
-
+//package prg1.assignment1
+//
 object FIB {
   //An implementation of the Fibonacci function using the definition of the Fibonacci number
   def fib_rec(n: Int): BigInt = n match {
@@ -9,7 +9,21 @@ object FIB {
 
   //An implementation of the Fibonacci function using iteration (tail recursion)
   def fib_itr(n: Int): BigInt = {
-    0
+    n match {
+      case 0 => 0
+      case 1 => 1
+      case _ =>{
+        var f0: BigInt = 0;
+        var f1: BigInt = 1;
+        var f2: BigInt = 1;
+        for (i <- 2 to n){
+          f2 = f0 + f1;
+          f0 = f1;
+          f1 = f2;
+        }
+        f2;
+      }
+    }
   }
 
   //An implementation of the Fibonacci function using matrix products
@@ -35,7 +49,7 @@ object FIB {
 
     bench(fib_rec, n, "fib_rec")
     bench(fib_itr, n, "fib_itr")
-    bench(fib_matrix, n, "fib_matrix")
-    bench(fib_polynomial, n, "fib_polynomial")
+    //bench(fib_matrix, n, "fib_matrix")
+    //bench(fib_polynomial, n, "fib_polynomial")
   }
 }
