973afcab6c3713479ff06404bb787f8835e66430
hiragaatsuya <hiraga.a.ab@m.titech.ac.jp>
2018-10-03 18:37:12 +0900

    1

--- src/assignment1-fibonacci.scala
+++ src/assignment1-fibonacci.scala
@@ -9,17 +9,45 @@ object FIB {
 
   //An implementation of the Fibonacci function using iteration (tail recursion)
   def fib_itr(n: Int): BigInt = {
-    0
+    def Ntimespl(n: Int,a: BigInt,b: BigInt): BigInt = n match{
+    	case 0 => b
+    	case _ => Ntimespl(n-1,a+b,a)
+    }
+    Ntimespl(n,1,0)
   }
 
   //An implementation of the Fibonacci function using matrix products
   def fib_matrix(n: Int): BigInt = {
-    0
+  	/*        a          b   
+  	A=[     b          c           ] として行列の積を計算をする
+  	*/
+  	def pow(n:Int,a:BigInt,b:BigInt,c:BigInt): (BigInt,BigInt,BigInt)= n match{
+  		case 0 => (1,0,1)
+  		case m if m%2==1 => pow((n-1)/2,a*a+b*b,a*b+b*c,b*b+c*c) match{
+  			case (x,y,z) => (a*x+b*y,a*y+b*z,b*y+c*z)
+  		}
+  		case _ => pow(n/2,a*a+b*b,a*b+b*c,b*b+c*c)
+  	}
+  	pow(n,1,1,0) match{
+  		case (a,b,c) => b
+  	}
   }
 
   //An implementation of the Fibonacci function using polynomial products
   def fib_polynomial(n: Int): BigInt = {
-    0
+  	/*
+  	初期のPが１次式、Qが２次式ならばP'o,P'e,Q'はそれぞれ１、１、２次以下の式となること、Qの０次の係数が帰納的に常に１となることを利用し、
+  	P=ax+b
+  	Q=cx^2+dx+1   とした。
+  	*/
+    def pol(n:Int,a:BigInt,b:BigInt,c:BigInt,d:BigInt): (BigInt,BigInt,BigInt,BigInt)= n match{
+    	case 0 => (a,b,c,d)
+    	case m if m%2==1 => pol(n/2,a*c,a-b*d,c*c,2*c-d*d)
+    	case _ => pol(n/2,b*c-a*d,b,c*c,2*c-d*d)
+    }
+    pol(n,1,0,-1,-1) match{
+    	case (a,b,c,d) => b
+    }
   }
 
   def bench(f: Int => BigInt, n: Int, name: String): Unit = {
@@ -39,3 +67,4 @@ object FIB {
     bench(fib_polynomial, n, "fib_polynomial")
   }
 }
+
--- test/assignment1-fibonaccitest.scala
+++ test/assignment1-fibonaccitest.scala
@@ -4,10 +4,27 @@ import org.scalatest._
 import FIB._
 
 class FIBTest extends FunSuite with Matchers {
-   test("FIB tests") {
-     fib_itr(2) should equal (fib_rec(2))
-   }
-
+   test("S_FIB tests") {
+    	for{i <- 0 to 20} fib_itr(i) should equal (fib_rec(i))
+    	for{i <- 0 to 20} {
+    		//println(i + "open!!")
+    		fib_matrix(i) should equal (fib_rec(i))
+    	}
+    	for{i <- 0 to 20} {
+    		//println(i + "open!!")
+    		fib_polynomial(i) should equal (fib_rec(i))
+    	}   
+    }
+    test("L_FIB tests") {
+    	for{i <- 5 to 20} {
+    		//println(i + "open!!")
+    		fib_matrix(1<<i) should equal (fib_itr(1<<i))
+    	}
+    	for{i <- 5 to 25} {
+    		//println(i + "open!!")
+    		fib_matrix(1<<i) should equal (fib_polynomial(1<<i))
+    	}   
+    }
 }
 
 
