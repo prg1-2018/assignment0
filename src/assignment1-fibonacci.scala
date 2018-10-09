1e61498cad6126df8c7997bef8a05af3c9498fff
Yoshimura Yusei <yoshimura.y.ag@m.titech.ac.jp>
2018-10-09 14:20:13 +0900

    finish

--- src/assignment1-fibonacci.scala
+++ src/assignment1-fibonacci.scala
@@ -9,12 +9,41 @@ object FIB {
 
   //An implementation of the Fibonacci function using iteration (tail recursion)
   def fib_itr(n: Int): BigInt = {
-    0
+    var m = n// NOTE:
+    var a:BigInt = 1
+    var b:BigInt = 0
+    var c:BigInt = 0
+    while(m >= 1){
+      c = b
+      b = a
+      a = a + c
+      m = m - 1
+    }
+    b
   }
 
   //An implementation of the Fibonacci function using matrix products
   def fib_matrix(n: Int): BigInt = {
-    0
+
+    def pow(l: List[BigInt], m: Int): List[BigInt] = {
+
+      def squ(ls: List[BigInt]): List[BigInt] = ls match{
+        case aa::ab::ba::bb::Nil => (aa*aa + ab*ba)::(aa*ab + ab*bb)::(ba*aa + bb*ab)::(ba*ab + bb*bb)::Nil
+      }
+
+      if(m == 0){
+        List(1, 0, 0, 1)
+      }else if(m % 2 == 1){
+        val List(aa, ab, ba, bb) = pow(squ(l), (m-1)/2)
+        val List(cc, cd, dc, dd) = l
+        (cc*aa + cd*ba)::(cc*ab + cd*bb)::(dc*aa + dd*ba)::(dc*ab + dd*bb)::Nil
+      }else{
+        pow(squ(l), m/2)
+      }
+    }
+
+    val List(_, _, ba, _) = pow(List(1, 1, 1, 0), n)
+    ba
   }
 
   //An implementation of the Fibonacci function using polynomial products
--- test/assignment1-fibonaccitest.scala
+++ test/assignment1-fibonaccitest.scala
@@ -5,10 +5,8 @@ import FIB._
 
 class FIBTest extends FunSuite with Matchers {
    test("FIB tests") {
-     fib_itr(2) should equal (fib_rec(2))
+     fib_itr(10) should equal (fib_rec(10))
+     fib_matrix(100) should equal (fib_itr(100))
    }
 
 }
-
-
-
