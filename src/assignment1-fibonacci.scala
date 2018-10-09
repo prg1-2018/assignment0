e39da3fdc9473d056d9f2830a38161a8e559919d
Sasagawa Takuya <sasagawa.t.ae@is-mac25.edu.c.titech.ac.jp>
2018-10-09 17:27:55 +0900

    imihumei

--- src/assignment1-fibonacci.scala
+++ src/assignment1-fibonacci.scala
@@ -78,7 +78,102 @@ object FIB {
 
   //An implementation of the Fibonacci function using polynomial products
   def fib_polynomial(n: Int): BigInt = {
-    0
+    def porec(m: Int, p: Array[BigInt], dp: Int, q: Array[BigInt], dq: Int): BigInt = {
+      //m次の係数を返す p:P(x), q:Q(x), dp:整式Pの係数を格納する配列の末尾のラベル dq:整式Qの係数を格納する配列の末尾のラベル
+      m match{
+        case 0 => p(0)
+        case _ =>{
+          def isEven(n : Int): Boolean = {
+            (n % 2) == 0
+          }
+          def pocon(a: Array[BigInt], da: Int, b: Array[BigInt], db: Int): Array[BigInt] = {
+            //整式の積
+            var c = new Array[BigInt](da+db+1);
+            for(i <- 0 to da+db){
+              c(i) = 0
+            }
+            for(i <- 0 to da){
+              for(j <- 0 to db){
+                c(i+j) = c(i+j) + a(i) * b(j)
+              }
+            }
+            c
+          }
+          def poev(r: Array[BigInt], dr: Int): Array[BigInt] = {
+            //整式のうち、偶数次数の係数のみ取り出す
+            var e = new Array[BigInt](dr/2+1);
+            for(i <- 0 to dr/2){
+              e(i) = r(2*i);
+            }
+            e
+          }
+          def podd(r: Array[BigInt], dr: Int): Array[BigInt] = {
+            //整式のうち、奇数次数の係数のみ取り出す
+            isEven(dr) match{
+              case true => {
+                var o = new Array[BigInt](dr/2)
+                for(i <- 0 to dr/2-1){
+                  o(i) = r(2*i + 1);
+                }
+                o
+              }
+              case false => {
+                var o = new Array[BigInt](dr/2+1);
+                for(i <- 0 to dr/2){
+                  o(i) = r(2*i + 1);
+                }
+                o
+              }
+            }
+          }
+          def revQ(qx: Array[BigInt], dqx: Int): Array[BigInt] = {
+            //Q(x)の係数からQ(-x)の係数を計算する
+            isEven(dqx) match{
+              case true => {
+                for(i <- 0 to dqx/2-1){
+                  qx(2*i + 1) = -1*qx(2*i + 1);
+                }
+                qx
+              }
+              case false => {
+                for(i <- 0 to dqx/2){
+                  qx(2*i + 1) = -1*qx(2*i + 1);
+                }
+                qx
+              }
+            }
+          }
+          isEven(m) match {
+            case true =>{
+              porec(m/2, poev(pocon(p, dp, revQ(q, dq), dq), dp+dq), (dp+dq)/2, poev(pocon(q, dq, revQ(q, dq),  dq), dq+dq), dq);
+            }
+            case false =>{
+              isEven(dp + dq) match{
+                case true =>{
+                  porec((m-1)/2, podd(pocon(p, dp, revQ(q, dq), dq), dp+dq), (dp+dq)/2-1, poev(pocon(q, dq, revQ(q, dq),  dq), dq+dq), dq);
+                }
+                case false =>{
+                  porec((m-1)/2, podd(pocon(p, dp, revQ(q, dq), dq), dp+dq), (dp+dq)/2, poev(pocon(q, dq, revQ(q, dq),  dq), dq+dq), dq);
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+    n match{
+      case 0 => 0
+      case _ => {
+        var p0 = new Array[BigInt](2);
+        var q0 = new Array[BigInt](3);
+        p0(0) = 0;
+        p0(1) = 1;
+        q0(0) = 1;
+        q0(1) = -1;
+        q0(2) = -1;
+        porec(n, p0, 1, q0, 2)
+      }
+    }
   }
 
   def bench(f: Int => BigInt, n: Int, name: String): Unit = {
