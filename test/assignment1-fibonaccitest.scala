e4859364d84ef252e3b976c8cc9fab97621b7346
Yoshimura Yusei <yoshimura.y.ag@m.titech.ac.jp>
2018-10-09 14:28:39 +0900

    finish

--- test/assignment1-fibonaccitest.scala
+++ test/assignment1-fibonaccitest.scala
@@ -5,7 +5,11 @@ import FIB._
 
 class FIBTest extends FunSuite with Matchers {
    test("FIB tests") {
+     fib_itr(0) should equal (fib_rec(0))
+     fib_itr(1) should equal (fib_rec(1))
      fib_itr(10) should equal (fib_rec(10))
+     fib_matrix(0) should equal (fib_itr(0))
+     fib_matrix(1) should equal (fib_itr(1))
      fib_matrix(100) should equal (fib_itr(100))
    }
 
