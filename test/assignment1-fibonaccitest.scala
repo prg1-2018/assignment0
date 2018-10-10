bbc520f0aa5262975f9188c56b4e4f04cb6d0810
hirosejoe <hirose.j.aa@m.titech.ac.jp>
2018-10-10 13:57:46 +0900

    aaa

--- test/assignment1-fibonaccitest.scala
+++ test/assignment1-fibonaccitest.scala
@@ -6,6 +6,7 @@ import FIB._
 class FIBTest extends FunSuite with Matchers {
    test("FIB tests") {
      fib_itr(2) should equal (fib_rec(2))
+     fib_matrix(100) should equal (fib_itr(100))
    }
 
 }
