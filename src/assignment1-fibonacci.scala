f585b2615ae33369b6c0fb1240279f6d028cccc2
Iida Yuto <iida.y.aj@is-mac22.edu.c.titech.ac.jp>
2018-10-10 13:42:55 +0900

    kadai

--- src/assignment1-fibonacci.scala
+++ src/assignment1-fibonacci.scala
@@ -23,16 +23,16 @@ object FIB {
   
   //An implementation of the Fibonacci function using matrix products
   def fib_matrix(n: Int): BigInt = {
-    def sexi(A:Array[Int],B:Array[Int]): Array[Int] = {
+    def sexi(A:Array[BigInt],B:Array[BigInt]): Array[BigInt] = {
      Array(A(0)*B(0)+A(1)*B(2),A(0)*B(1)+A(1)*B(3),A(2)*B(0)+A(3)*B(2),A(2)*B(1)+A(3)*B(3))
      }
-    def pow(A:Array[Int],n:Int): Array[Int] = {
-        val I = Array(1,0,0,1)
+    def pow(A:Array[BigInt],n:Int): Array[BigInt] = {
+        val I: Array[BigInt] = Array(1,0,0,1)
         if(n==0) return I
         else if (n%2==1) return sexi(A,pow(sexi(A,A),(n-1)/2))
         else return pow(sexi(A,A),n/2) 
     }
-    val A = Array(1,1,1,0)
+    val A: Array[BigInt] = Array(1,1,1,0)
     val C = pow(A,n)
     return C(2)
   }
