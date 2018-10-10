307092eb8ff740e9f5eb43d942890c8264950d34
Iida Yuto <iida.y.aj@is-mac22.edu.c.titech.ac.jp>
2018-10-10 11:22:00 +0900

    課題

--- src/assignment1-fibonacci.scala
+++ src/assignment1-fibonacci.scala
@@ -8,14 +8,35 @@ object FIB {
   }
 
   //An implementation of the Fibonacci function using iteration (tail recursion)
-  def fib_itr(n: Int): BigInt = {
-    0
+  def fib_itr(n: Int): BigInt =  {
+    var m = n
+    var a: BigInt = 1
+    var b: BigInt = 0
+    while(m >= 1){
+    	var c: BigInt = a
+    	a = a + b 
+    	b = c
+    	m = m-1
+    }
+    return b
   }
-
+  
   //An implementation of the Fibonacci function using matrix products
   def fib_matrix(n: Int): BigInt = {
-    0
+    def sexi(A:Array[Int],B:Array[Int]): Array[Int] = {
+     Array(A(0)*B(0)+A(1)*B(2),A(0)*B(1)+A(1)*B(3),A(2)*B(0)+A(3)*B(2),A(2)*B(1)+A(3)*B(3))
+     }
+    def pow(A:Array[Int],n:Int): Array[Int] = {
+        val I = Array(1,0,0,1)
+        if(n==0) return I
+        else if (n%2==1) return sexi(A,pow(sexi(A,A),(n-1)/2))
+        else return pow(sexi(A,A),n/2) 
+    }
+    val A = Array(1,1,1,0)
+    val C = pow(A,n)
+    return C(2)
   }
+  
 
   //An implementation of the Fibonacci function using polynomial products
   def fib_polynomial(n: Int): BigInt = {
