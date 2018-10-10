2c518165fa7cf20d56d611bdc548394e3b038a05
Iida Yuto <iida.y.aj@is-mac22.edu.c.titech.ac.jp>
2018-10-10 13:58:49 +0900

    kakak

--- test/assignment1-fibonaccitest.scala
+++ test/assignment1-fibonaccitest.scala
@@ -7,7 +7,10 @@ class FIBTest extends FunSuite with Matchers {
    test("FIB tests") {
      fib_itr(2) should equal (fib_rec(2))
    }
-
+   test("FIB test") {
+	fib_matrix(100) should equal (fib_itr(100))
+   }
+   
 }
 
 
