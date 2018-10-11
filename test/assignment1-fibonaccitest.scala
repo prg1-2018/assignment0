e9e4053964027fd800270af959371737bb2f178c
Sasagawa Takuya <sasagawa.t.ae@is-mac07.edu.c.titech.ac.jp>
2018-10-11 17:53:09 +0900

    test age

--- test/assignment1-fibonaccitest.scala
+++ test/assignment1-fibonaccitest.scala
@@ -6,9 +6,9 @@ import FIB._
 class FIBTest extends FunSuite with Matchers {
    test("FIB tests") {
      fib_itr(2) should equal (fib_rec(2))
+     fib_itr(50) should equal (fib_rec(50))
+     fib_rec(100) should equal (fib_matrix(100))
+     fib_matrix(1000) should equal(fib_polynomial(1000))
    }
 
 }
-
-
-
