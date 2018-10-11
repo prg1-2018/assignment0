e8bec0fc0a0e37784ed57c8cb568287b6bee92c3
kawamura rui <kawamura.r.af@m.titech.ac.jp>
2018-10-11 15:18:23 +0900

    test修正

--- test/assignment1-fibonaccitest.scala
+++ test/assignment1-fibonaccitest.scala
@@ -5,6 +5,10 @@ import FIB._
 
 class FIBTest extends FunSuite with Matchers {
    test("FIB tests") {
+     fib_itr(0) should equal (fib_rec(0))
+     fib_matrix(0) should equal (fib_rec(0))
+     fib_polynomial(0) should equal (fib_rec(0))
+
      fib_itr(2) should equal (fib_rec(2))
      fib_matrix(2) should equal (fib_rec(2))
      fib_polynomial(2) should equal (fib_rec(2))
