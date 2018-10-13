f896521c730b32fdc360ea917803e32445753bcf
joehirose <edop6anlq5blue9dopa@gmail.com>
2018-10-13 19:57:45 +0900

    last

--- test/assignment1-fibonaccitest.scala
+++ test/assignment1-fibonaccitest.scala
@@ -3,13 +3,35 @@ package prg1.assignment1
 import org.scalatest._
 import FIB._
 
-class FIBTest extends FunSuite with Matchers {
-   test("FIB tests") {
-     fib_itr(2) should equal (fib_rec(2))
+class FIBTestRecToItr extends FunSuite with Matchers {
+   test("FIB test Rec To Itr.") {
+   	var n=10
+   	while (n>=0){
+     fib_itr(n) should equal (fib_rec(n))
+     n=n-1
+     }
+     var m=100
+     while (m>=0){
      fib_matrix(100) should equal (fib_itr(100))
+     m=m-1
+     }
    }
 
 }
+class FIBTestItrToMat extends FunSuite with Matchers {
+   test("FIB test Itr To Mat.") {
+   	var n=10
+   	while (n>=0){
+     fib_itr(n) should equal (fib_rec(n))
+     n=n-1
+     }
+     var m=100
+     while (m>=0){
+     fib_matrix(100) should equal (fib_itr(100))
+     m=m-1
+     }
+   }
 
+}
 
 
