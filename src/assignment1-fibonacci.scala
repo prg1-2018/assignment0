e7b5b2046b9513cc6f49944ed4e137604b777bd6
joehirose <edop6anlq5blue9dopa@gmail.com>
2018-10-13 19:35:30 +0900

    last

--- src/assignment1-fibonacci.scala
+++ src/assignment1-fibonacci.scala
@@ -24,14 +24,14 @@ object FIB {
   //An implementation of the Fibonacci function using matrix products
   def fib_matrix(n: Int): BigInt = {
     def mul(A: Array[BigInt], B: Array[BigInt]): Array[BigInt] = {
-    Array(A(0)*B(0)+A(1)*B(1),A(0)*B(1)+A(1)*B(2),A(1)*B(1)+A(2)*B(2))
-}
+        Array(A(0)*B(0)+A(1)*B(1),A(0)*B(1)+A(1)*B(2),A(1)*B(1)+A(2)*B(2))
+    }
     val D:Array[BigInt] = Array(1,1,0)
   	def pow(C:Array[BigInt],m:Int): Array[BigInt] = m match {
   	  case 0 => Array(1,0,1)
   	  case _ => if (m%2==1){mul(C,pow(mul(C,C),(m-1)/2))}
   	  else{pow(mul(C,C),m/2)}
-  	  }
+  	}
     pow(D,n)(1)
   }
 
