06c51af6ff2c3bb4ed1647737a2a1192cb682971
WadaTakuya <wada.t.ap@m.titech.ac.jp>
2018-10-15 08:54:45 +0900

    難しい

--- src/assignment1-fibonacci.scala
+++ src/assignment1-fibonacci.scala
@@ -7,15 +7,46 @@ object FIB {
     case _ => fib_rec(n-1) + fib_rec(n-2)
   }
 
-  //An implementation of the Fibonacci function using iteration (tail recursion)
+  //An implementation of the Fibonacci function using iteration (tail recursion) 
   def fib_itr(n: Int): BigInt = {
-    0
+    var m = n
+    var a : BigInt = 1
+    var b : BigInt = 0
+    var c : BigInt = 1
+    while (m >= 1) {
+        c = a
+        a = a + b
+        b = c
+        m = m - 1
+    }
+    return b
   }
 
   //An implementation of the Fibonacci function using matrix products
+  case class Matrix(a11: BigInt, a12: BigInt, a21: BigInt, a22: BigInt)
   def fib_matrix(n: Int): BigInt = {
-    0
-  }
+    def mult(a: Matrix, b: Matrix): Matrix = {
+    	val Matrix(x, y, z, w) = a
+    	val Matrix(o, p, q, r) = b
+    	return Matrix(x*o+y*q, x*p+y*r, z*o+w*q, z*p+w*r)
+    }
+    def aaa(a: Matrix): Matrix = {
+    	val Matrix(x, y, z, w) = a
+    	return Matrix(x*x+y*z, x*y+y*w, x*z+z*w, y*z+w*w)
+    }
+    def pow(a: Matrix, n: Int): Matrix = {
+    	n match {
+    		case 0 => return Matrix(1,0,0,1)
+    		case _ => val c = n % 2
+    						c match {
+    						case 1 => return mult(a,pow(aaa(a),(n-1)/2))
+    						case _ => return pow(aaa(a),n/2)
+    						}
+    	}
+    }
+    val Matrix(e, f, g, h) = pow(Matrix(1,1,1,0),n)
+    return g
+   }
 
   //An implementation of the Fibonacci function using polynomial products
   def fib_polynomial(n: Int): BigInt = {
@@ -31,7 +62,7 @@ object FIB {
   }
 
   def main(arg: Array[String]): Unit = {
-    val n = 10
+    val n = 100
 
     bench(fib_rec, n, "fib_rec")
     bench(fib_itr, n, "fib_itr")
--- test/assignment1-fibonaccitest.scala
+++ test/assignment1-fibonaccitest.scala
@@ -5,10 +5,18 @@ import FIB._
 
 class FIBTest extends FunSuite with Matchers {
    test("FIB tests") {
-     fib_itr(2) should equal (fib_rec(2))
+   	fib_itr(0) should equal (fib_rec(0))
+    fib_itr(2) should equal (fib_rec(2))
+    fib_itr(10) should equal (fib_rec(10))
+    }
+   test("FIB tests matrix") {
+   	fib_matrix(0) should equal (fib_itr(0))
+    fib_matrix(2) should equal (fib_itr(2))
+    fib_matrix(10) should equal (fib_itr(10))
+    fib_matrix(50) should equal (fib_itr(50))
+    fib_matrix(100) should equal (fib_itr(100))
+    fib_matrix(500) should equal (fib_itr(500))
    }
-
-}
-
+   }
 
 
