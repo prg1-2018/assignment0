1d0a28454ad76a95e067bb3b0253b82dc71171fe
rie maskawa <riemaskawa@rienoMacBook-Air.local>
2018-10-15 01:10:58 +0900

    完成

--- test/assignment1-fibonaccitest.scala
+++ test/assignment1-fibonaccitest.scala
@@ -8,7 +8,7 @@ class FIBTest extends FunSuite with Matchers {
      fib_itr(2) should equal (fib_rec(2))
      fib_itr(10) should equal (fib_rec(10))
      fib_itr(15) should equal (fib_rec(15))
-     fib_itr(1000) should equal (fib_rec(20))
+     fib_itr(100) should equal (fib_rec(20))
      fib_matrix(2) should equal (fib_rec(2))
      fib_matrix(10) should equal (fib_rec(10))
      fib_matrix(15) should equal (fib_rec(15))
