811366348866200baf1e2bf4d9233e51b4a49f4c
MakiShuhei <shuheititech@gmail.com>
2018-10-14 16:45:42 +0900

    課題を提出します

--- src/assignment1-fibonacci.scala
+++ src/assignment1-fibonacci.scala
@@ -9,12 +9,36 @@ object FIB {
 
   //An implementation of the Fibonacci function using iteration (tail recursion)
   def fib_itr(n: Int): BigInt = {
-    0
+  	var m = n
+  	var a : BigInt = 1
+   	var b : BigInt = 0
+   	var c : BigInt = 1
+    while (m >= 1) {
+    	c = a
+    	a = c+b
+   		b = c 
+   	    m = m-1
+   	}
+   	return b
   }
 
   //An implementation of the Fibonacci function using matrix products
-  def fib_matrix(n: Int): BigInt = {
-    0
+def fib_matrix(n: Int): BigInt = {
+	def pow(A:List[BigInt] ,b: BigInt): List[BigInt] = {
+		if(b == 0) List(1,0,0,1)
+		else if(b%2 == 1) mlt(A,pow(mlt(A,A),(b-1)/2))
+		//A*pow(A^2,(b-1)/2)
+		else pow(mlt(A,A),b/2)
+		//pow(A^2,b/2)
+    }
+	def mlt(A:List[BigInt], B:List[BigInt]): List[BigInt] = {
+		val c0 : BigInt = A(0) * B(0) + A(1) * B(2)
+		val c1 : BigInt = A(0) * B(1) + A(1) * B(3)
+		val c2 : BigInt = A(2) * B(0) + A(3) * B(2)
+		val c3 : BigInt = A(2) * B(1) + A(3) * B(3)
+		return List(c0,c1,c2,c3)
+	}
+	pow(List(1,1,1,0),n)(2)
   }
 
   //An implementation of the Fibonacci function using polynomial products
@@ -32,7 +56,7 @@ object FIB {
 
   def main(arg: Array[String]): Unit = {
     val n = 10
-
+    
     bench(fib_rec, n, "fib_rec")
     bench(fib_itr, n, "fib_itr")
     bench(fib_matrix, n, "fib_matrix")
--- test/assignment1-fibonaccitest.scala
+++ test/assignment1-fibonaccitest.scala
@@ -5,7 +5,8 @@ import FIB._
 
 class FIBTest extends FunSuite with Matchers {
    test("FIB tests") {
-     fib_itr(2) should equal (fib_rec(2))
+     fib_itr(20) should equal (fib_rec(20))
+     fib_matrix(20) should equal (fib_itr(20))
    }
 
 }
