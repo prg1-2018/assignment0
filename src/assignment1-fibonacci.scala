5b878a3dd998ccdb987d2915b22bf3ffe18bebce
朝倉ひかる <asakurahikaru@asakurahikarunoMacBook-Air.local>
2018-10-15 03:34:03 +0900

    fib

--- src/assignment1-fibonacci.scala
+++ src/assignment1-fibonacci.scala
@@ -9,12 +9,31 @@ object FIB {
 
   //An implementation of the Fibonacci function using iteration (tail recursion)
   def fib_itr(n: Int): BigInt = {
-    0
+    var m = n
+    var a:BigInt = 1
+    var b:BigInt = 0
+    while(m >= 1){
+    	a = a+b
+    	b = a-b
+    	m = m-1
+    }
+    return b
   }
 
   //An implementation of the Fibonacci function using matrix products
   def fib_matrix(n: Int): BigInt = {
-    0
+    case class Matrix(a11: BigInt, a12: BigInt, a21: BigInt, a22: BigInt)
+    def mult(a:Matrix , b:Matrix):Matrix = {
+        val Matrix(x, y, z, w) = a
+        val Matrix(o, p, q, r) = b
+        Matrix(x*o+y*q , x*p+y*r , z*o+w*q , z*p+w*r)
+    }
+    def pow(A:Matrix , n:Int):Matrix = {
+        if (n==0) Matrix(1, 0, 0, 1)
+        else if (n%2==1) mult(A, pow(mult(A, A), (n-1)/2))
+        else pow(mult(A, A), n/2)   
+    }
+   pow(Matrix(1, 1, 1, 0), n).a21
   }
 
   //An implementation of the Fibonacci function using polynomial products
