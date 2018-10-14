305b1eb3718954ae2bedee346c07277604aa3e49
rie maskawa <riemaskawa@rienoMacBook-Air.local>
2018-10-15 01:08:27 +0900

    完成

--- src/assignment1-fibonacci.scala
+++ src/assignment1-fibonacci.scala
@@ -9,12 +9,30 @@ object FIB {
 
   //An implementation of the Fibonacci function using iteration (tail recursion)
   def fib_itr(n: Int): BigInt = {
-    0
+     var m = n
+     var a:BigInt = 1
+     var b:BigInt = 0
+     while(m >= 1){
+       a = a + b
+       b = a - b
+       m = m - 1
+    }
+    b
   }
 
   //An implementation of the Fibonacci function using matrix products
   def fib_matrix(n: Int): BigInt = {
-    0
+  	def multi(A: List[BigInt], B:List[BigInt]): List[BigInt] = {
+  		List(A(0)*B(0)+A(1)*B(2),A(0)*B(1)+A(1)*B(3),A(2)*B(0)+A(3)*B(2),A(2)*B(1)+A(3)*B(3))
+  	}
+    def pow(A: List[BigInt], n: Int) : List[BigInt] = {
+    	if (n==0) List(1,0,0,1)
+    	else if (n%2==1) multi(A,pow(multi(A,A),(n-1)/2))
+    	else pow(multi(A,A),n/2)
+    }
+    pow(List(1,1,1,0),n) match{
+    	case List(a,b,c,d) => c
+    }
   }
 
   //An implementation of the Fibonacci function using polynomial products
--- test/assignment1-fibonaccitest.scala
+++ test/assignment1-fibonaccitest.scala
@@ -6,6 +6,13 @@ import FIB._
 class FIBTest extends FunSuite with Matchers {
    test("FIB tests") {
      fib_itr(2) should equal (fib_rec(2))
+     fib_itr(10) should equal (fib_rec(10))
+     fib_itr(15) should equal (fib_rec(15))
+     fib_itr(1000) should equal (fib_rec(20))
+     fib_matrix(2) should equal (fib_rec(2))
+     fib_matrix(10) should equal (fib_rec(10))
+     fib_matrix(15) should equal (fib_rec(15))
+     fib_matrix(100) should equal (fib_rec(20))
    }
 
 }
