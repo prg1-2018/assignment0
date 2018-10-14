a20b0ad2cb7588b3ab0f4e66b81c649e01ead645
kazuki ikemori <ikemori.k.aa@m.titech.ac.jp>
2018-10-14 18:06:00 +0900

    source test

--- src/assignment1-fibonacci.scala
+++ src/assignment1-fibonacci.scala
@@ -9,17 +9,62 @@ object FIB {
 
   //An implementation of the Fibonacci function using iteration (tail recursion)
   def fib_itr(n: Int): BigInt = {
-    0
+    var m = n
+    var a :BigInt = 1
+    var b :BigInt = 0
+    var temp :BigInt = 0
+    while(m>=1){
+      temp = b
+      b = a
+      a = a + temp
+      m = m-1
+    }
+    return b
   }
 
   //An implementation of the Fibonacci function using matrix products
+case class Matrix(a: BigInt,b: BigInt,c: BigInt,d: BigInt)
   def fib_matrix(n: Int): BigInt = {
-    0
+
+    def pow(matrix: Matrix,n: Int): Matrix = {
+
+      def products(matrix1: Matrix ,matrix2: Matrix) : Matrix = {
+        val Matrix(a1,b1,c1,d1) = matrix1
+        val Matrix(a2,b2,c2,d2) = matrix2
+        Matrix(a1*a2+b1*c2,a1*b2+b1*d2,c1*a2+d1*c2,c1*b2+d1*d2)
+      }
+
+      if(n==0) Matrix(1,0,0,1)
+      else if(n%2!=0) products(matrix,pow(products(matrix,matrix),(n-1)/2))
+      else pow(products(matrix,matrix),n/2)
+    }
+
+    val Matrix(a,b,c,d) = pow(Matrix(1,1,1,0),n)
+    return c
   }
 
   //An implementation of the Fibonacci function using polynomial products
+case class Polynomial(a: BigInt,b: BigInt,c: BigInt)
   def fib_polynomial(n: Int): BigInt = {
-    0
+
+    def multPoly(poly1: Polynomial,poly2: Polynomial,eventrue: Boolean): Polynomial = {
+      val Polynomial(a0,a1,a2) = poly1
+      val Polynomial(b0,b1,b2) = poly2
+
+      if(eventrue) Polynomial(a0*b0,a2*b0-a1*b1+a0*b2,a2*b2)
+      else Polynomial(a1*b0-a0*b1,-a2*b1+a1*b2,0)
+    }
+
+    def fib(p: Polynomial,q: Polynomial,n: Int): BigInt = {
+      val evenq : Polynomial = multPoly(q,q,true)
+      val Polynomial(p0,p1,p2) = p
+
+      if(n==0) p0
+      else if(n%2==0) fib(multPoly(p,q,true),evenq,n/2)
+      else fib(multPoly(p,q,false),evenq,(n-1)/2)
+    }
+
+    fib(Polynomial(0,1,0),Polynomial(1,-1,-1),n)
   }
 
   def bench(f: Int => BigInt, n: Int, name: String): Unit = {
--- test/assignment1-fibonaccitest.scala
+++ test/assignment1-fibonaccitest.scala
@@ -4,11 +4,60 @@ import org.scalatest._
 import FIB._
 
 class FIBTest extends FunSuite with Matchers {
-   test("FIB tests") {
+   test("FIB tests ITR") {
+     fib_itr(0) should equal (fib_rec(0))
+     fib_itr(1) should equal (fib_rec(1))
      fib_itr(2) should equal (fib_rec(2))
+     fib_itr(3) should equal (fib_rec(3))
+     fib_itr(4) should equal (fib_rec(4))
+     fib_itr(5) should equal (fib_rec(5))
+     fib_itr(10) should equal (fib_rec(10))
+     fib_itr(15) should equal (fib_rec(15))
+     fib_itr(20) should equal (fib_rec(20))
+     fib_itr(25) should equal (fib_itr(25))
+     fib_itr(30) should equal (fib_rec(30))
+     fib_itr(35) should equal (fib_rec(35))
+     fib_itr(40) should equal (fib_rec(40))
+   }
+
+   test("FIB tests MATRIX"){
+     fib_matrix(0) should equal (fib_itr(0))
+     fib_matrix(1) should equal (fib_itr(1))
+     fib_matrix(2) should equal (fib_itr(2))
+     fib_matrix(3) should equal (fib_itr(3))
+     fib_matrix(4) should equal (fib_itr(4))
+     fib_matrix(5) should equal (fib_itr(5))
+     fib_matrix(11) should equal (fib_itr(11))
+     fib_matrix(21) should equal (fib_itr(21))
+     fib_matrix(31) should equal (fib_itr(31))
+     fib_matrix(41) should equal (fib_itr(41))
+     fib_matrix(51) should equal (fib_itr(51))
+     fib_matrix(61) should equal (fib_itr(61))
+     fib_matrix(71) should equal (fib_itr(71))
+     fib_matrix(81) should equal (fib_itr(81))
+     fib_matrix(91) should equal (fib_itr(91))
+     fib_matrix(101) should equal (fib_itr(101))
+
+  }
+
+   test("FIB tests POLYNOMIAL"){
+     fib_polynomial(0) should equal (fib_matrix(0))
+     fib_polynomial(1) should equal (fib_matrix(1))
+     fib_polynomial(2) should equal (fib_matrix(2))
+     fib_polynomial(3) should equal (fib_matrix(3))
+     fib_polynomial(4) should equal (fib_matrix(4))
+     fib_polynomial(5) should equal (fib_matrix(5))
+     fib_polynomial(12) should equal (fib_matrix(12))
+     fib_polynomial(22) should equal (fib_matrix(22))
+     fib_polynomial(32) should equal (fib_matrix(32))
+     fib_polynomial(42) should equal (fib_matrix(42))
+     fib_polynomial(52) should equal (fib_matrix(52))
+     fib_polynomial(62) should equal (fib_matrix(62))
+     fib_polynomial(72) should equal (fib_matrix(72))
+     fib_polynomial(82) should equal (fib_matrix(82))
+     fib_polynomial(92) should equal (fib_matrix(92))
+     fib_polynomial(102) should equal (fib_matrix(102))
+     fib_polynomial(100001) should equal (fib_matrix(100001))
    }
 
 }
-
-
-
