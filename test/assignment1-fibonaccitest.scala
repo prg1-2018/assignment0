a690a289a7df730377ae2aa7ed5d25eb69b32ea4
Kawaguchi Yuma <kawaguchi.y.ac@is-mac11.edu.c.titech.ac.jp>
2018-10-15 16:11:33 +0900

    assignment

--- test/assignment1-fibonaccitest.scala
+++ test/assignment1-fibonaccitest.scala
@@ -5,9 +5,17 @@ import FIB._
 
 class FIBTest extends FunSuite with Matchers {
    test("FIB tests") {
+   	 fib_itr(0) should equal (fib_rec(0))
+   	 fib_itr(1) should equal (fib_rec(1))
      fib_itr(2) should equal (fib_rec(2))
+     fib_itr(10) should equal (fib_rec(10))
+     fib_matrix(0) should equal (fib_itr(0))
+     fib_matrix(1) should equal (fib_itr(1))
+     fib_matrix(10) should equal (fib_itr(10))
+     fib_matrix(50) should equal (fib_itr(50))
+     fib_matrix(1000) should equal (fib_itr(1000))
+     fib_matrix(10000) should equal (fib_itr(10000))
    }
-
 }
 
 
