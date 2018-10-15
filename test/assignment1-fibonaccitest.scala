92344ead64e3fcf403fe3651642b9dd707c7685d
NakasaKazuhiro <nakasa.k.aa@m.titech.ac.jp>
2018-10-15 17:30:38 +0900

    assignment1

--- test/assignment1-fibonaccitest.scala
+++ test/assignment1-fibonaccitest.scala
@@ -6,8 +6,9 @@ import FIB._
 class FIBTest extends FunSuite with Matchers {
    test("FIB tests") {
      fib_itr(2) should equal (fib_rec(2))
+     fib_itr(0) should equal (fib_rec(0))
+     fib_matrix(0) should equal (fib_itr(0))
    }
-
 }
 
 
