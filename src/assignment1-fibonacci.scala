a19f2802e63e3959cc4b361b1ae54da742d9bfbb
Ayaka Igarashi <igarashi.a.ab@m.titech.ac.jp>
2018-10-15 17:21:42 +0900

    kansei

--- src/assignment1-fibonacci.scala
+++ src/assignment1-fibonacci.scala
@@ -1,5 +1,6 @@
 package prg1.assignment1
 
+
 object FIB {
   //An implementation of the Fibonacci function using the definition of the Fibonacci number
   def fib_rec(n: Int): BigInt = n match {
@@ -9,13 +10,46 @@ object FIB {
 
   //An implementation of the Fibonacci function using iteration (tail recursion)
   def fib_itr(n: Int): BigInt = {
-    0
+    var m=n
+    var a:BigInt=1
+    var b:BigInt=0
+    def hozyo(m:Int,a:BigInt,b:BigInt): BigInt ={
+    	if(m==0){b}
+    	else{hozyo(m-1,a+b,a)}
+    }
+    hozyo(m,a,b)
   }
 
   //An implementation of the Fibonacci function using matrix products
   def fib_matrix(n: Int): BigInt = {
-    0
+    
+    def gyoretu(a:List[BigInt],n:Int): List[BigInt]={
+		a match{
+			case(List(a1,a2,a3,a4))=>{
+		if(n==0){List(0,0,0,0)}
+    	else if(n==1){List(a1,a2,a3,a4)}
+    	else if(n%2==1){kakezan(List(a1,a2,a3,a4),gyoretu(List(a1*a1+a2*a3,a1*a2+a2*a4,a1*a3+a4*a3,a2*a3+a4*a4),(n-1)/2))}
+    	else{gyoretu(List(a1*a1+a2*a3,a1*a2+a2*a4,a1*a3+a4*a3,a2*a3+a4*a4),n/2)}
+    	}}
+    	
+    	
+    	
+    }
+    
+    def kakezan(a:List[BigInt],b:List[BigInt]):List[BigInt]={
+			(a,b) match{
+				case(List(a1,a2,a3,a4),List(b1,b2,b3,b4))=>{
+	List(a1*b1+a2*b3,a1*b2+a2*b4,a3*b1+a4*b3,a3*b2+a4*b4)
+	}}}
+	
+    def toridasu(a:List[BigInt]):BigInt={
+		a match{
+			case(List(a1,a2,a3,a4))=>a3
+			}
+		}
+    toridasu(gyoretu(List(1,1,1,0),n))
   }
+
 
   //An implementation of the Fibonacci function using polynomial products
   def fib_polynomial(n: Int): BigInt = {
@@ -31,7 +65,7 @@ object FIB {
   }
 
   def main(arg: Array[String]): Unit = {
-    val n = 10
+    val n = 0
 
     bench(fib_rec, n, "fib_rec")
     bench(fib_itr, n, "fib_itr")
