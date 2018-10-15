bcb1aaa28e6582a9baeba0a12b6ad9aadbfca265
Yuki Asano <asano.y.ah@m.titech.ac.jp>
2018-10-15 13:01:31 +0900

    test

--- test/assignment1-fibonaccitest.scala
+++ test/assignment1-fibonaccitest.scala
@@ -5,7 +5,16 @@ import FIB._
 
 class FIBTest extends FunSuite with Matchers {
    test("FIB tests") {
+   	 fib_itr(0) should equal (fib_rec(0))
      fib_itr(2) should equal (fib_rec(2))
+     fib_itr(10) should equal (fib_rec(10))
+     fib_itr(30) should equal (fib_rec(30))
+     fib_matrix(0) should equal (fib_itr(0))
+     fib_matrix(2) should equal (fib_itr(2))
+     fib_matrix(10) should equal (fib_itr(10))
+     fib_matrix(29) should equal (fib_itr(29))
+     fib_matrix(50) should equal (fib_itr(50))
+     fib_matrix(70) should equal (fib_itr(70))
    }
 
 }
