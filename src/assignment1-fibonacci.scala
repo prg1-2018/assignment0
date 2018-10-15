fa1cc3183a59de230eca1ff47183fefb51dab809
Kawaguchi Yuma <kawaguchi.y.ac@is-mac11.edu.c.titech.ac.jp>
2018-10-15 16:10:03 +0900

    assignment

--- src/assignment1-fibonacci.scala
+++ src/assignment1-fibonacci.scala
@@ -9,12 +9,34 @@ object FIB {
 
   //An implementation of the Fibonacci function using iteration (tail recursion)
   def fib_itr(n: Int): BigInt = {
-    0
+  	def calc(a:BigInt, b:BigInt ,m:Int): BigInt = m match {
+  	case 0  => b
+  	case _ => calc(a+b, a, m-1)
+  	}
+  	calc(1,0,n)
   }
 
   //An implementation of the Fibonacci function using matrix products
   def fib_matrix(n: Int): BigInt = {
-    0
+	// 2*2と2*2 or 2*1の行列計積をもとめる
+  	def  arrprd(lst1: List[BigInt] ,lst2: List[BigInt]): List[BigInt] = (lst1,lst2) match {
+  		case (a00::a01::a10::a11::Nil,b00::b01::b10::b11::Nil) =>
+  		List(a00*b00+a01*b10, a00*b01+a01*b11, a10*b00+a11*b10, a10*b01+a11*b11)
+  		case (a00::a01::a10::a11::Nil,b00::b10::Nil) =>
+  		List(a00*b00+a01*b10, a10*b00+a11*b10)
+  		case _ => Nil
+  	}
+  	def pow(A: List[BigInt],m: Int): List[BigInt] = m match {
+  		case 0 => List(1,0,0,1)
+  		case _ => m%2 match {
+  			case 1 => arrprd(A, pow(arrprd(A, A) , (m-1)/2))
+  			case _ => pow(arrprd(A, A), m/2)
+  		}
+  	}
+  	arrprd(pow(List(1,1,1,0), n), List(1,0)) match {
+  		case a::b::Nil => b
+  		case _ => -1
+  	}
   }
 
   //An implementation of the Fibonacci function using polynomial products
