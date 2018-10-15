6cb6fea833094131a0106587a622c6bedac00ed5
Kawaguchi Yuma <kawaguchi.y.ac@is-mac11.edu.c.titech.ac.jp>
2018-10-15 16:13:57 +0900

    assignment

--- src/assignment1-fibonacci.scala
+++ src/assignment1-fibonacci.scala
@@ -18,7 +18,7 @@ object FIB {
 
   //An implementation of the Fibonacci function using matrix products
   def fib_matrix(n: Int): BigInt = {
-	// 2*2と2*2 or 2*1の行列計積をもとめる
+	// 2*2と2*2 or 2*1の行列積をもとめる
   	def  arrprd(lst1: List[BigInt] ,lst2: List[BigInt]): List[BigInt] = (lst1,lst2) match {
   		case (a00::a01::a10::a11::Nil,b00::b01::b10::b11::Nil) =>
   		List(a00*b00+a01*b10, a00*b01+a01*b11, a10*b00+a11*b10, a10*b01+a11*b11)
