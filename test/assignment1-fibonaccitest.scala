4cf8059f0310cc75ec4d5c9db93d442bee9ba7ca
naoyafurudono <furudono.n.aa@m.titech.ac.jp>
2018-10-17 15:34:14 +0900

    mat

--- test/assignment1-fibonaccitest.scala
+++ test/assignment1-fibonaccitest.scala
@@ -4,11 +4,14 @@ import org.scalatest._
 import FIB._
 
 class FIBTest extends FunSuite with Matchers {
-   test("FIB tests") {
-     fib_itr(2) should equal (fib_rec(2))
+   test("FIB tests itr") {
+     fib_itr(10) should equal (fib_rec(10))
+     fib_itr(0) should equal (fib_rec(0))
+ }
+
+   test("FIB tests matrix"){
+       for(n<- 0 to 20){
+           fib_matrix(n*n) should equal (fib_itr(n*n))
+       }
    }
-
 }
-
-
-
