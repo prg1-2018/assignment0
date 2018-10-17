7772bbffdfe1ee2f47f94b7843986c5c775b76b0
Yutaro-Sanada <40193574+Yutaro-Sanada@users.noreply.github.com>
2018-10-17 16:46:43 +0900

    Update assignment1-fibonaccitest.scala

--- test/assignment1-fibonaccitest.scala
+++ test/assignment1-fibonaccitest.scala
@@ -5,16 +5,15 @@ import FIB._
 
 class FIBTest extends FunSuite with Matchers {
    test("FIB tests") {
+	fib_itr(0) should equal (fib_rec(0))
+	fib_matrix(0) should equal (fib_rec(0))
+	
    	fib_itr(2) should equal (fib_rec(2))
    	fib_matrix(2) should equal (fib_rec(2))
    	
-   	fib_itr(50) should equal (fib_rec(50))
-   	fib_matrix(50) should equal (fib_rec(50))
+   	fib_itr(10) should equal (fib_rec(10))
+   	fib_matrix(10) should equal (fib_rec(10))
    	
-   	fib_itr(100) should equal (fib_rec(100))
-   	fib_matrix(100) should equal (fib_rec(100))
+   	fib_matrix(100) should equal (fib_itr(100))
     }
 }
-
-
-
