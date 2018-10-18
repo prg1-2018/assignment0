eacb155b0be71c7f51956269b79ccbe6820ff501
Kazuya-Kawahara <kawahara.k.ad@m.titech.ac.jp>
2018-10-18 12:28:22 +0900

    テスト三つずつにした

--- test/assignment1-fibonaccitest.scala
+++ test/assignment1-fibonaccitest.scala
@@ -8,6 +8,7 @@ class FIBTest extends FunSuite with Matchers {
      fib_itr(2) should equal (fib_rec(2))
      fib_itr(20) should equal (fib_rec(20))
      fib_itr(0) should equal (fib_rec(0))
+     fib_matrix(10) should equal (fib_itr(10))
      fib_matrix(100) should equal (fib_itr(100))
      fib_matrix(0) should equal (fib_itr(0))
    }
