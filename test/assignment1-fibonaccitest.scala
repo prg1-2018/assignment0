d0081654a03d3bf8fdbf55bc40d2753ede6dd1c1
Sasagawa Takuya <sasagawa.t.ae@is-mac38.edu.c.titech.ac.jp>
2018-10-18 19:44:15 +0900

    add for loop in tests

--- test/assignment1-fibonaccitest.scala
+++ test/assignment1-fibonaccitest.scala
@@ -9,13 +9,15 @@ class FIBTest extends FunSuite with Matchers {
      fib_matrix(0) should equal (fib_itr(0))
      fib_polynomial(0) should equal (fib_matrix(0))
 
-     fib_itr(2) should equal (fib_rec(2))
-     fib_itr(10) should equal (fib_rec(10))
-     fib_matrix(10) should equal (fib_itr(10))
-     fib_matrix(20) should equal (fib_itr(20))
-     fib_polynomial(20) should equal (fib_matrix(20))
-     fib_polynomial(30) should equal (fib_matrix(30))
-     fib_polynomial(50) should equal (fib_matrix(50))
+     for(i <- 1 to 10){
+       fib_itr(i) should equal (fib_rec(i))
+     }
+     for(i <- 1 to 20){
+       fib_matrix(i) should equal (fib_itr(i))
+     }
+     for(i <- 1 to 50){
+       fib_polynomial(i) should equal (fib_matrix(i))
+     }
      fib_polynomial(100) should equal (fib_matrix(100))
      fib_polynomial(1000) should equal (fib_matrix(1000))
    }
