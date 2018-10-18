b5aa7d6346b3cfacfaa2c5999ec53ad557c7b478
Sasagawa Takuya <sasagawa.t.ae@is-mac38.edu.c.titech.ac.jp>
2018-10-18 19:28:42 +0900

    add tests for f(0) and add more tests

--- test/assignment1-fibonaccitest.scala
+++ test/assignment1-fibonaccitest.scala
@@ -5,10 +5,19 @@ import FIB._
 
 class FIBTest extends FunSuite with Matchers {
    test("FIB tests") {
+     fib_itr(0) should equal (fib_rec(0))
+     fib_rec(0) should equal (fib_matrix(0))
+     fib_matrix(0) should equal (fib_polynomial(0))
+
      fib_itr(2) should equal (fib_rec(2))
-     fib_itr(50) should equal (fib_rec(50))
-     fib_rec(100) should equal (fib_matrix(100))
-     fib_matrix(1000) should equal(fib_polynomial(1000))
+     fib_itr(10) should equal (fib_rec(10))
+     fib_rec(10) should equal (fib_matrix(10))
+     fib_rec(20) should equal (fib_matrix(20))
+     fib_matrix(20) should equal (fib_polynomial(20))
+     fib_matrix(30) should equal (fib_matrix(30))
+     fib_matrix(50) should equal (fib_matrix(50))
+     fib_matrix(100) should equal (fib_matrix(100))
+     fib_matrix(1000) should equal (fib_matrix(1000))
    }
 
 }
