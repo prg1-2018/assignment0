f34b4c25c10abe1b0adca82b37a8bd7fcc6a219a
MakiShuhei <shuheititech@gmail.com>
2018-10-20 19:10:07 +0900

    最提出

--- test/assignment1-fibonaccitest.scala
+++ test/assignment1-fibonaccitest.scala
@@ -5,8 +5,12 @@ import FIB._
 
 class FIBTest extends FunSuite with Matchers {
    test("FIB tests") {
+     fib_itr(0) should equal (fib_rec(0))
+     fib_matrix(0) should equal (fib_itr(0))
      fib_itr(20) should equal (fib_rec(20))
      fib_matrix(20) should equal (fib_itr(20))
+     fib_matrix(200) should equal (fib_itr(200))
+     fib_matrix(2000) should equal (fib_itr(2000))
    }
 
 }
